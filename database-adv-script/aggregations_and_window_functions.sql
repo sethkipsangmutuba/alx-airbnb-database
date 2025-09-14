10e3b8966657ac07e619b5e48842e051307f5f9c
Seth Kipsang Mutuba <61922630+sethkipsangmutuba@users.noreply.github.com>
2025-09-14 23:30:52 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -9,11 +9,12 @@ GROUP BY u.user_id, u.name
 ORDER BY total_bookings DESC;
 
 -- 2. Rank properties based on total bookings using window function
+
 SELECT 
     p.property_id,
     p.name AS property_name,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM Properties p
 LEFT JOIN Bookings b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name
